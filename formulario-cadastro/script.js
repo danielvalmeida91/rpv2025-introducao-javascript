31633b4a4bd591b4803ba53e91b57dddf99b1875
danielvalmeida91 <danielvalmeida91@gmail.com>
2025-12-16 21:07:59 -0300

    feat: avancando no formulario de cadastro

--- formulario-cadastro/script.js
+++ formulario-cadastro/script.js
@@ -10,13 +10,54 @@ const email = document.querySelector("#email")
 const sexo = document.querySelector("#sexo")
 const dataNascimento = document.querySelector("#data-nascimento")
 
+const spanErrorNome = document.getElementById('text-error-nome')
 formCadastro.addEventListener('submit', (event) => {
     event.preventDefault()
     console.log('infos', nome.value, email.value, sexo.value, dataNascimento.value)
     
-    if( nome.value === '' || email.value === '' || sexo.value === '' || dataNascimento.value === ''){
-        return alert('Existem campos obrigatórios não preenchidos !')
+    const arrayComOsCamposQuePossuemErro = []
+    
+    if(nome.value === '') {
+        nome.style.border = '1px solid red'
+        spanErrorNome.style.display = 'inline'
+        arrayComOsCamposQuePossuemErro.push('nome')
+    }else {
+        nome.style.border = ''
+        spanErrorNome.style.display = 'none'
     }
+    if(email.value === '') {
+        email.style.border = '1px solid red'
+        arrayComOsCamposQuePossuemErro.push('email')
+    }else {
+        email.style.border = ''
+    }
+    if(sexo.value === '') {
+        sexo.style.border = '1px solid red'
+        arrayComOsCamposQuePossuemErro.push('sexo')
+    }else {
+        sexo.style.border = ''
+    }
+    if(dataNascimento.value === '') {
+        dataNascimento.style.border = '1px solid red'
+        arrayComOsCamposQuePossuemErro.push('dataNascimento')
+    }else {
+        dataNascimento.style.border = ''
+    }
+
+    if(arrayComOsCamposQuePossuemErro.length > 0) return;
+
+    alert('Passou da validação inicial')
+
+    console.log('array de erros', arrayComOsCamposQuePossuemErro)
+    
+    // if( nome.value === '' || email.value === '' || sexo.value === '' || dataNascimento.value === ''){
+    //     // mesma coisa que nome.value === '' ? nome.style.border = '1px solid red' : nome.style.border = ''
+    //     email.value === '' ? email.style.border = '1px solid red' : email.style.border = ''
+    //     sexo.value === '' ? sexo.style.border = '1px solid red' : sexo.style.border = ''
+    //     dataNascimento.value === '' ? dataNascimento.style.border = '1px solid red' : dataNascimento.style.border = ''
+    //     // return alert('Existem campos obrigatórios não preenchidos !')
+    //     return;
+    // }
 
     const possuiNomeESobrenome = nome.value.trim().split(' ').filter(item => item.length > 0).length > 1
 
